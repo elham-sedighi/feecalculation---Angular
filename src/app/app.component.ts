16e4e81195c4b94f1c2a7a4d154704e0974ce6d6
l.sedighi70@gmail.com <l.sedighi70@gmail.com>
2021-09-23 00:09:07 +0330

    cash out natural fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,12 @@ import {concatMap, switchMap} from "rxjs/operators";
 import {forkJoin, fromEvent, Observable, Subscription} from "rxjs";
 import {Operation} from './model/operation';
 import * as moment from 'moment';
+
+moment.updateLocale('en', {
+  week: {
+    dow: 1
+  }
+})
 import {FeeConfig, feeConfigs} from './model/feeConfig';
 import {OperationType} from './model/operationType';
 import {UserType} from './model/userType';
@@ -16,7 +22,8 @@ import {UserType} from './model/userType';
 export class AppComponent implements OnDestroy, OnInit, AfterViewInit {
   title = 'Gini';
   subscriptions = new Subscription();
-  userOperationRecords: Map<number, { week: number, amount: number }> = new Map<number, { week: number; amount: number }>();
+  userOperationRecords: Map<number, { date: moment.Moment, amount: number }> =
+    new Map<number, { date: moment.Moment, amount: number }>();
   feeConfigs: feeConfigs = {cash_in: null, cash_out_natural: null, cash_out_juridical: null};
 
   @ViewChild('calculateFeesBtn', {read: ElementRef}) calculateFeesButton: ElementRef;
@@ -52,19 +59,97 @@ export class AppComponent implements OnDestroy, OnInit, AfterViewInit {
     return this.http.get<Operation[]>(path);
   }
 
-  resetUserRecord(operation: Operation) {
-    this.userOperationRecords.set(operation.user_id, {week: moment().week(), amount: 0})
-  }
+  /*setUserRecord(operation: Operation) {
+    const previous_amount = !!this.userOperationRecords.get(operation.user_id)?.amount ?
+      this.userOperationRecords.get(operation.user_id)?.amount : 0;
+    this.userOperationRecords.set(operation.user_id, {
+      date: moment(operation.date, 'YYYY-MM-DD'),
+      week: moment(operation.date, 'YYYY-MM-DD').week(),
+      amount: previous_amount + operation.operation.amount
+    })
+  }*/
 
   formatter = new Intl.NumberFormat('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2});
 
-  calculateFee(operation: Operation): number {
-    let fee = 0;
-    const oldRecord = this.userOperationRecords.get(operation.user_id);
-    if (!oldRecord?.amount || !oldRecord?.week) {
-      this.resetUserRecord(operation);
+  calculateFee(opr: Operation): number {
+    if (opr.type === OperationType.cash_in) {
+      return this.calculateCashInFee(opr.operation.amount);
     }
+    return this.calculateCashOutFee(opr);
+  }
 
+  calculateCashInFee(amount: number) {
+    let fee = parseFloat(this.formatter.format((amount * this.feeConfigs.cash_in.percents) / 100));
+    fee = fee > this.feeConfigs.cash_in.max.amount ? this.feeConfigs.cash_in.max.amount : fee;
+    return fee;
+  }
+
+  calculateCashOutFee(opr: Operation) {
+    if (opr.user_type == UserType.juridical) {
+      return this.calculateCashOutJuridicalFee(opr);
+    }
+    return this.calculateCashOutNatural(opr);
+  }
+
+  calculateCashOutJuridicalFee(opr: Operation) {
+    let fee = parseFloat(this.formatter.format((opr.operation.amount * this.feeConfigs.cash_out_juridical.percents) / 100));
+    fee = fee < this.feeConfigs.cash_out_juridical.min.amount ? this.feeConfigs.cash_in.min.amount : fee;
+    return fee;
+  }
+
+  calculateCashOutNatural(opr: Operation) {
+    const existedAmount = this.getExistedAmount(opr.user_id, moment(opr.date, 'YYYY-MM-DD'));
+    let fee = this.calculateCashOutNaturalFee(existedAmount, opr.operation.amount);
+    this.addOprToMap(opr);
+    return fee;
+  }
+
+  getExistedAmount(userId: number, date: moment.Moment) {
+    let oldRecord = this.userOperationRecords.get(userId);
+    if (!oldRecord) {
+      return 0;
+    }
+    if (oldRecord.date.week() === date.week()) {
+      return oldRecord.amount;
+    }
+    return 0;
+  }
+
+  calculateCashOutNaturalFee(existedAmount: number, newAmount: number) {
+    let fee = 0;
+    const allAmounts = existedAmount + newAmount;
+    if (allAmounts > this.feeConfigs.cash_out_natural.week_limit.amount) {
+      const effectiveAmount = newAmount - this.calculateExceededAmount(existedAmount);
+      fee = parseFloat(this.formatter.format(effectiveAmount *
+        this.feeConfigs.cash_out_natural.percents * 0.01));
+    }
+    return fee;
+  }
+
+  calculateExceededAmount(existedAmount: number): number {
+    if (existedAmount - this.feeConfigs.cash_out_natural.week_limit.amount > 0) {
+      return 0;
+    }
+    return this.feeConfigs.cash_out_natural.week_limit.amount - existedAmount;
+  }
+
+  addOprToMap(opr: Operation) {
+    let oldRecord = this.userOperationRecords.get(opr.user_id);
+    const opr_Date = moment(opr.date, 'YYYY-MM-DD');
+    if (!oldRecord) {
+      this.userOperationRecords.set(opr.user_id, {date: opr_Date, amount: opr.operation.amount});
+    } else {
+      if (oldRecord.date.week() === opr_Date.week()) {
+        oldRecord.amount = oldRecord.amount + opr.operation.amount;
+      } else {
+        this.userOperationRecords.set(opr.user_id, {date: opr_Date, amount: opr.operation.amount});
+      }
+    }
+  }
+
+
+  /*calculateFee(operation: Operation): number {
+    let fee = 0;
     if (operation.type === OperationType.cash_in) {
       fee = parseFloat(this.formatter.format((operation.operation.amount * this.feeConfigs.cash_in.percents) / 100));
       fee = fee > this.feeConfigs.cash_in.max.amount ? this.feeConfigs.cash_in.max.amount : fee;
@@ -73,18 +158,24 @@ export class AppComponent implements OnDestroy, OnInit, AfterViewInit {
         fee = parseFloat(this.formatter.format((operation.operation.amount * this.feeConfigs.cash_out_juridical.percents) / 100));
         fee = fee < this.feeConfigs.cash_out_juridical.min.amount ? this.feeConfigs.cash_in.min.amount : fee;
       } else {
-        //let dd = moment(operation.date, 'YYYY-MM-DD').week();
-        if (oldRecord.week === moment(operation.date, 'YYYY-MM-DD').week()) {
+        let oldRecord = this.userOperationRecords.get(operation.user_id);
+        if (!oldRecord?.amount || !oldRecord?.week) {
+          this.setUserRecord(operation);
+        }
+        oldRecord = this.userOperationRecords.get(operation.user_id);
+        if (oldRecord.week === moment(operation.date, 'YYYY-MM-DD').week()
+          && oldRecord.date !== moment(operation.date, 'YYYY-MM-DD')) {
           const accumulatedAmount = oldRecord.amount + operation.operation.amount;
           fee = accumulatedAmount < this.feeConfigs.cash_out_natural.week_limit.amount ? 0 : parseFloat(this.formatter.format((operation.operation.amount * this.feeConfigs.cash_out_natural.percents) / 100));
+          this.setUserRecord(operation);
         } else {
-          this.resetUserRecord(operation);
+          this.setUserRecord(operation);
           this.calculateFee(operation);
         }
       }
     }
     return fee;
-  }
+  }*/
 
   ngAfterViewInit(): void {
     //listen to user clicks
@@ -100,10 +191,11 @@ export class AppComponent implements OnDestroy, OnInit, AfterViewInit {
       concatMap(inputData => {
         console.log('start calculating fees...')
         return inputData.map(operation => {
-          return this.calculateFee(operation);
+          console.log(this.calculateFee(operation));
+          return true;
         });
       })
-    ).subscribe((fee: number) => {
+    ).subscribe((fee: boolean) => {
       console.log(fee);
     }));
   }
